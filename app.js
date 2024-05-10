84bac9681879768c2380f44b95809fa72b25e642
kxxdev <phgang@ya.ru>
2024-05-10 19:33:37 +0500

    test commit

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ const {
 const { User, Message, GuildMember, ThreadMember, Channel } = Partials;
 
 import { loadEvents } from './Handlers/eventHandler.js';
+//
 import { loadCommands } from './Handlers/commandsHanlder.js';
 
 const client = new Client({
